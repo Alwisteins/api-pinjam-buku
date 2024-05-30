6cc95b1027303bd5cea7e10d800b72689d36c2a0
unknown <alwyyourbae@gmail.com>
2024-05-31 06:32:40 +0700

    fix borrow

--- src/controller/booksController.js
+++ src/controller/booksController.js
@@ -3,28 +3,28 @@ import membersModel from "../model/membersModel.js";
 
 const borrow = async (req, res) => {
   try {
-    const { bookName, memberName } = req.body;
+    const { bookName, member } = req.body;
 
     // 1). validate req body
-    if (!bookName || !memberName) {
+    if (!bookName || !member || !member.name || !member.code) {
       return res
         .status(400)
         .json({ message: "Please provide the required data" });
     }
 
     // 2). validate member
-    const member = membersModel.getMemberByName(memberName);
-    if (!member) {
+    const isMemberExist = await membersModel.getMemberByName(member);
+    if (!isMemberExist) {
       return res.status(404).json({ message: "Member not found" });
     }
-    if (member.borrowedBooks.length >= 2) {
+    if (isMemberExist.borrowedBooks.length >= 2) {
       return res.status(403).json({
         message: "Member has reached the maximum limit for borrowing books",
       });
     }
 
-    // 2). validate book
-    const book = booksModel.getBookByName(bookName);
+    // 3). validate book
+    const book = await booksModel.getBookByName(bookName);
     if (!book) {
       return res.status(404).json({ message: "Book not found" });
     }
@@ -34,12 +34,13 @@ const borrow = async (req, res) => {
         .json({ message: "Book was borrowed by other member" });
     }
 
-    const data = await booksModel.borrow(book, member);
+    // 4). perform borrow transaction
+    const data = await booksModel.borrow(book, isMemberExist);
 
-    res.status(200).json({ message: "successfuly borrow a book", data });
+    res.status(200).json({ message: "Successfully borrowed a book", data });
   } catch (err) {
     console.log(err);
-    res.status(200).json({ err: err.message });
+    res.status(500).json({ err: err.message });
   }
 };
 
