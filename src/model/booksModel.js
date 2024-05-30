58f8a10b6d1ccb20bbffb4f30ac94383c858c129
unknown <alwyyourbae@gmail.com>
2024-05-31 05:43:19 +0700

    add findByName & borrow model

---
+++ src/model/booksModel.js
@@ -0,0 +1,40 @@
+import prisma from "../config/database.js";
+
+const findBookByName = (bookName) => {
+  return prisma.book.findUnique({
+    where: { title: bookName },
+    include: { borrowedBy: true },
+  });
+};
+
+const borrow = (book, member) => {
+  return prisma.$transaction(async (trx) => {
+    // 1) update book, change the stock & borrowedBy
+    const updatedBook = await trx.book.update({
+      where: { title: book },
+      data: { stock: { decrement: 1 } },
+    });
+
+    // 2) create borrowed book entry
+    const borrowedBook = await trx.borrowedBook.create({
+      data: {
+        borrowedAt: new Date(),
+        book: {
+          connect: { code: book.code },
+          member: { connect: { code: member.code } },
+        },
+      },
+    });
+
+    // 1) update member, change the borrowedBooks
+    const updatedMember = await trx.member.update({
+      where: { code: member.code },
+      data: { borrowedBooks: { connect: { id: borrowedBook.id } } },
+    });
+
+    return { updatedBook, updatedMember, borrowedBook };
+  });
+};
+
+const booksModel = { findBookByName, borrow };
+export default booksModel;
