4d0c184c74d34e836d37c1ba58c67cf52d486a05
unknown <alwyyourbae@gmail.com>
2024-05-31 06:34:11 +0700

    fix book model

--- src/model/booksModel.js
+++ src/model/booksModel.js
@@ -11,18 +11,17 @@ const borrow = (book, member) => {
   return prisma.$transaction(async (trx) => {
     // 1) update book, change the stock & borrowedBy
     const updatedBook = await trx.book.update({
-      where: { title: book },
+      where: { title: book.title },
       data: { stock: { decrement: 1 } },
+      include: { borrowedBy: true },
     });
 
     // 2) create borrowed book entry
     const borrowedBook = await trx.borrowedBook.create({
       data: {
         borrowedAt: new Date(),
-        book: {
-          connect: { code: book.code },
-          member: { connect: { code: member.code } },
-        },
+        book: { connect: { code: book.code } },
+        member: { connect: { code: member.code } },
       },
     });
 
@@ -30,9 +29,10 @@ const borrow = (book, member) => {
     const updatedMember = await trx.member.update({
       where: { code: member.code },
       data: { borrowedBooks: { connect: { id: borrowedBook.id } } },
+      include: { borrowedBooks: true },
     });
 
-    return { updatedBook, updatedMember, borrowedBook };
+    return { updatedBook, updatedMember };
   });
 };
 
