4a71b9b719abb18ed62071131aea4b486882b30c
unknown <alwyyourbae@gmail.com>
2024-05-31 22:55:38 +0700

    add members router

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import booksRouter from '../src/routes/bookRoutes.js';
+import membersRouter from '../src/routes/memberRoutes.js'
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json())
 
 //router
 app.use('/api/v1/books', booksRouter)
+app.use('/api/v1/members', membersRouter)
 
 export default app;
